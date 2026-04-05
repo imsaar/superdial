b0f4ec2647b3adce9da6519144dca5d7204c1d64
Ali Rizvi <aliabbasrizvi@gmail.com>
2026-04-05 13:31:57 -0700

    chore: add .gitignore and remove .pio build artifacts from tracking

--- .pio/build/esp32c3_supermini/FrameworkArduino/cbuf.cpp.d
+++
@@ -1,3 +0,0 @@
-.pio/build/esp32c3_supermini/FrameworkArduino/cbuf.cpp.o: \
- /Users/arizvi/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
- /Users/arizvi/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
--- .pio/build/esp32c3_supermini/FrameworkArduino/libb64/cdecode.c.d
+++
@@ -1,3 +0,0 @@
-.pio/build/esp32c3_supermini/FrameworkArduino/libb64/cdecode.c.o: \
- /Users/arizvi/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cdecode.c \
- /Users/arizvi/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cdecode.h
